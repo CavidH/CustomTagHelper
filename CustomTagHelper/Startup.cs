a784017e864c49ed219c9f68c2900c4eef77f83b
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2021-12-31 17:32:02 +0400

    :rocket:

--- CustomTagHelper/Startup.cs
+++ CustomTagHelper/Startup.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CustomTagHelper
 {
@@ -19,23 +15,18 @@ namespace CustomTagHelper
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            
             app.UseStaticFiles();
 
             app.UseRouting();
