7936cb50c3c5d1d5de2b4ca8a61c6c81363719d5
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2021-12-31 18:00:58 +0400

    Custom Tag Helper Done

--- CustomTagHelper/TagHelpers/EmailTagHelper.cs
+++ CustomTagHelper/TagHelpers/EmailTagHelper.cs
@@ -4,5 +4,21 @@ namespace CustomTagHelper.TagHelpers
 {
     public class EmailTagHelper:TagHelper
     {
+        public string Mail { get; set; }
+        public string Display { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("href",$"mailto:{Mail}");
+            output.Content.Append(Display);
+
+           // base.Process(context, output);
+        }
     }
+
+    //public class SecondCustomTagHelper : TagHelper
+    //{
+
+    //}
 }
